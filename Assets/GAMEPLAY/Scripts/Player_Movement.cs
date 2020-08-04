6f1d9141a3fd08d459ad0485f4099cec770100f7
MirOoll <47189644+MirOoll@users.noreply.github.com>
2020-08-04 19:43:55 +0300

    0408

---
+++ Assets/GAMEPLAY/Scripts/Player_Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+	public Rigidbody2D rb;
+	public float moveSpeed = 5f;
+	Vector2 movement;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		movement.x = Input.GetAxis("Horizontal"); 
+	}
+
+	void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
+}
